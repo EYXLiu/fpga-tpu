d9e66f107de6b9f8777a7bb51992f36884fe7553
Evan Liu <evanyxliu@gmail.com>
2026-09-05 13:57:02 -0700

    added uart RX/TX display

--- tang_nano_20k/memory_test/memory.py
+++ tang_nano_20k/memory_test/memory.py
@@ -8,23 +8,33 @@ ser = serial.Serial(PORT, BAUD, timeout=3)
 
 def read(addr):
     packet = b'R' + struct.pack(">H", addr)
+
+    print(f"TX: {packet.hex(' ')}")
+
     ser.write(packet)
+
     data = ser.read(4)
 
-    if (len(data) != 4):
+    print(f"RX: {data.hex(' ')}")
+
+    if len(data) != 4:
         print(f"Expected 4 bytes, got {len(data)}")
-        print(data)
         return
-    
+
     return struct.unpack(">I", data)[0]
 
 def write(addr, data):
     packet = b'W' + struct.pack(">H", addr) + struct.pack(">I", data)
+
+    print(f"TX: {packet.hex(' ')}")
+
     ser.write(packet)
-    
+
     ack = ser.read(1)
+
+    print(f"RX: {ack.hex(' ')}")
+
     if ack != b'\x06':
-        print(ack)
         raise RuntimeError("Write failed")
 
 write(0, 123)
